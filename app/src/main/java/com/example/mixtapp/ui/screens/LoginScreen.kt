1436808d9012d6fa070f9e9642b9780c5b5efdbc
Andres <andresemr2006@outlook.es>
2026-08-13 22:29:10 -0500

    Correccion de los composables de la pantalla de login

--- app/src/main/java/com/example/mixtapp/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/mixtapp/ui/screens/LoginScreen.kt
@@ -12,20 +12,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.mixtapp.Screen
 import com.example.mixtapp.ui.theme.*
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(
-    onLoginSuccess: () -> Unit,
-    onNavigateToSignUp: () -> Unit,
     modifier: Modifier = Modifier // Primer parámetro opcional
 ) {
-    var email by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
-    var errorMessage by remember { mutableStateOf<String?>(null) }
 
     Column(
         modifier = modifier
@@ -35,108 +32,157 @@ fun LoginScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Mixtapp",
-            color = PrimaryMaroon,
-            fontSize = 42.sp,
-            fontWeight = FontWeight.Bold
-        )
+
+        NombreApp()
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        Text(
-            text = "Sign In",
-            color = TextLight,
-            fontSize = 26.sp,
-            fontWeight = FontWeight.Bold
-        )
+        Titulo(texto= "Sign in")
 
         Spacer(modifier = Modifier.height(6.dp))
 
-        Text(
-            text = "Enter your account to keep rating albums and following friends.",
-            color = Color.Gray,
-            fontSize = 12.sp,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
+        TextoDescripcion(texto = "Enter your account to keep rating albums and following friends.")
 
         Spacer(modifier = Modifier.height(32.dp))
 
-        // Campo de Email
-        OutlinedTextField(
-            value = email,
-            onValueChange = {
-                email = it
-                errorMessage = null
-            },
-            label = { Text("Email", color = Color.LightGray) },
-            placeholder = { Text("Enter your Email", color = Color.Gray) },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            shape = RoundedCornerShape(12.dp),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = PrimaryMaroon,
-                unfocusedBorderColor = Color.Transparent,
-                focusedContainerColor = SurfaceCard,
-                unfocusedContainerColor = SurfaceCard,
-                focusedLabelColor = AccentPink
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        // Campo de Contraseña
-        OutlinedTextField(
-            value = password,
-            onValueChange = {
-                password = it
-                errorMessage = null
-            },
-            label = { Text("Password", color = Color.LightGray) },
-            singleLine = true,
-            visualTransformation = PasswordVisualTransformation(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            shape = RoundedCornerShape(12.dp),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = PrimaryMaroon,
-                unfocusedBorderColor = Color.Transparent,
-                focusedContainerColor = SurfaceCard,
-                unfocusedContainerColor = SurfaceCard,
-                focusedLabelColor = AccentPink
-            ),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        errorMessage?.let { error ->
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = error, color = MaterialTheme.colorScheme.error, fontSize = 12.sp)
-        }
-
-        Spacer(modifier = Modifier.height(28.dp))
-
-        // Botón LOGIN
-        Button(
-            onClick = {
-                if (email.contains("@") && password.length >= 6) {
-                    onLoginSuccess()
-                } else {
-                    errorMessage = "Ingresa un correo válido y contraseña de mínimo 6 caracteres."
-                }
-            },
-            colors = ButtonDefaults.buttonColors(containerColor = PrimaryMaroon),
-            shape = RoundedCornerShape(24.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-        ) {
-            Text(text = "LOGIN", color = Color.White, fontWeight = FontWeight.Bold, fontSize = 16.sp)
-        }
+        // Campos de registro
+        FormularioRegistro()
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        TextButton(onClick = onNavigateToSignUp) {
+        DivisorOr()
+
+        TextButton(onClick = {/* TO DO */}) {
             Text(text = "Don't have an Account? Sign up", color = AccentPink, fontSize = 13.sp)
         }
+
     }
 }
+
+@Composable
+fun NombreApp(
+    modifier: Modifier = Modifier
+){
+    Text(
+        modifier = modifier,
+        text = "Mixtapp",
+        color = PrimaryMaroon,
+        fontSize = 42.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun Titulo(
+    modifier: Modifier = Modifier,
+    texto: String
+){
+    Text(
+        text = texto,
+        color = TextLight,
+        fontSize = 26.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun TextoDescripcion(
+    modifier: Modifier = Modifier,
+    texto: String
+){
+    Text(
+        text = texto,
+        color = Color.Gray,
+        fontSize = 12.sp,
+        modifier = Modifier.padding(horizontal = 16.dp)
+    )
+}
+
+@Composable
+fun FormularioRegistro(
+    modifier: Modifier = Modifier
+){
+    // Campo de Email
+    OutlinedTextField(
+        value = "",
+        onValueChange = {},
+        label = { Text("Email", color = Color.LightGray) },
+        placeholder = { Text("Enter your Email", color = Color.Gray) },
+        shape = RoundedCornerShape(12.dp),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = PrimaryMaroon,
+            unfocusedBorderColor = Color.Transparent,
+            focusedContainerColor = SurfaceCard,
+            unfocusedContainerColor = SurfaceCard,
+            focusedLabelColor = AccentPink
+        ),
+        modifier = Modifier.fillMaxWidth()
+    )
+
+    Spacer(modifier = Modifier.height(16.dp))
+
+    // Campo de Contraseña
+    OutlinedTextField(
+        value = "",
+        onValueChange = {},
+        label = { Text("Password", color = Color.LightGray) },
+        shape = RoundedCornerShape(12.dp),
+        colors = OutlinedTextFieldDefaults.colors(
+            focusedBorderColor = PrimaryMaroon,
+            unfocusedBorderColor = Color.Transparent,
+            focusedContainerColor = SurfaceCard,
+            unfocusedContainerColor = SurfaceCard,
+            focusedLabelColor = AccentPink
+        ),
+        modifier = Modifier.fillMaxWidth()
+    )
+
+    Spacer(modifier = Modifier.height(28.dp))
+
+    // Botón LOGIN
+
+    Button(
+        onClick = {/* TO DO*/},
+        colors = ButtonDefaults.buttonColors(containerColor = PrimaryMaroon),
+        shape = RoundedCornerShape(24.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Text(text = "LOGIN", color = Color.White, fontWeight = FontWeight.Bold, fontSize = 16.sp)
+    }
+
+}
+
+@Composable
+fun DivisorOr(
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = modifier
+    ) {
+        HorizontalDivider(
+            modifier = Modifier.weight(1f),
+            color = Color.Gray
+        )
+
+        Text(
+            text = "OR",
+            modifier = Modifier.padding(horizontal = 12.dp),
+            color = Color.Gray
+        )
+
+        HorizontalDivider(
+            modifier = Modifier.weight(1f),
+            color = Color.Gray
+        )
+    }
+
+}
+
+@Composable
+@Preview
+fun LoginScreenPreview(){
+    LoginScreen()
+}
+
