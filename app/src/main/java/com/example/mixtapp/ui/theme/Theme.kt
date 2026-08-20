d8a54daedd61e9cf042d1ebb129be158eea4b302
Andres <andresemr2006@outlook.es>
2026-08-19 20:27:42 -0500

    Fix: Correccion de las strings y los colores

--- app/src/main/java/com/example/mixtapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/mixtapp/ui/theme/Theme.kt
@@ -57,10 +57,3 @@ fun MixtappTheme(
         content = content
     )
 }
-
-val DarkBackground = Color(0xFF191616)
-val SurfaceCard = Color(0xFF330512)
-val PrimaryMaroon = Color(0xFFA02249)
-val SecondaryPlum = Color(0xFF67122C)
-val AccentPink = Color(0xFFF17692)
-val TextLight = Color(0xFFEDECEC)
