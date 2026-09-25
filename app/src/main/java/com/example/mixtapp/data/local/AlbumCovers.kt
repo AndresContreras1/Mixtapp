9972d8123001f42b9c3938440c1f07dac2564b82
Laura-Ap37 <lsofia_aponte@javeriana.edu.co>
2026-09-25 12:00:33 -0500

    update: album covers save in storage

--- app/src/main/java/com/example/mixtapp/data/local/AlbumCovers.kt
+++ app/src/main/java/com/example/mixtapp/data/local/AlbumCovers.kt
@@ -1,11 +1,11 @@
 package com.example.mixtapp.data.local
 
 object AlbumCovers {
-    const val TEATRO_DIRA = "https://picsum.photos/seed/teatrodira/600"
-    const val RUSH = "https://picsum.photos/seed/rush/600"
-    const val FINISTERRA = "https://picsum.photos/seed/finisterra/600"
-    const val FROM_ZERO = "https://picsum.photos/seed/fromzero/600"
-    const val TOXICITY = "https://picsum.photos/seed/toxicity/600"
-    const val RANDOM_ACCESS_MEMORIES = "https://picsum.photos/seed/randomaccessmemories/600"
-    const val THE_BLACK_PARADE = "https://picsum.photos/seed/theblackparade/600"
+    const val TEATRO_DIRA = "https://firebasestorage.googleapis.com/v0/b/mixtapp-720eb.firebasestorage.app/o/AlbumCovers%2Fteatro_maneskin.jpg?alt=media&token=9e41a947-0dd9-44ef-8860-683ff60b552f"
+    const val RUSH = "https://firebasestorage.googleapis.com/v0/b/mixtapp-720eb.firebasestorage.app/o/AlbumCovers%2Frush.jpg?alt=media&token=dfa6f793-2e0d-4841-a9dd-ad8a694ffc95"
+    const val FINISTERRA = "https://firebasestorage.googleapis.com/v0/b/mixtapp-720eb.firebasestorage.app/o/AlbumCovers%2Ffinisterrajpg.jpg?alt=media&token=2cca7c9e-2fda-4e73-aa79-f83daa1b4bf0"
+    const val FROM_ZERO = "https://firebasestorage.googleapis.com/v0/b/mixtapp-720eb.firebasestorage.app/o/AlbumCovers%2Ffrom_zerojpg.jpg?alt=media&token=3f558c04-c40c-423b-99c0-3ea5f75fc5d5"
+    const val TOXICITY = "https://firebasestorage.googleapis.com/v0/b/mixtapp-720eb.firebasestorage.app/o/AlbumCovers%2FSystemofaDownToxicityalbumcover.jpg?alt=media&token=363ac3da-1009-4d3e-8a05-aea753ba7198"
+    const val RANDOM_ACCESS_MEMORIES = "https://firebasestorage.googleapis.com/v0/b/mixtapp-720eb.firebasestorage.app/o/AlbumCovers%2Fab67616d0000b2739b9b36b0e22870b9f542d937.jpg?alt=media&token=0ee0193b-c1b3-40d5-b010-bf2491da1852"
+    const val THE_BLACK_PARADE = "https://firebasestorage.googleapis.com/v0/b/mixtapp-720eb.firebasestorage.app/o/AlbumCovers%2FBlackparadecover.jpg?alt=media&token=caee5e07-a118-4b14-a596-76f7b4070f80"
 }
